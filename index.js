8b33500c8c2f32cb3c60bc8c27331b291570c06f
Barbara Liau <bj.liau@gmail.com>
2017-04-12 13:02:02 -0600

    Code for you

--- index.js
+++ index.js
@@ -3,38 +3,60 @@
 // MODULES
 var express = require('express');
 var bodyParser = require('body-parser');
-
-// FAUX-DATABASE
-var movies = [{
-  "title": "Pride and Prejudice",
-  "director": "Jane Austen"
-}, {
-  "title": "Ocean's Eleven",
-  "director": "George Clooney"
-}, {
-  "title": "Serenity",
-  "director": "Joss Whedon"
-}, {
-  "title": "Kill Bill",
-  "director": "Quentin Tarentino"
-}];
+var movies = require('./models/movies.js');
+var MoviesController = require('./controllers/movies_controllers.js');
 
 // INSTANTIATE EXPRESS
 var app = express();
 var port = 9870;
 
-// ENDPOINTS
-app.get('/movies', function (req, res, next) {
-  res.status(200).send({
-    movies: movies,
-    message: 'Here are your movies'
-  });
+// MIDDLEWARE - happens on every request
+// app.use(function(req, res, next) {
+//  // do things
+    // call next() to call the next function
+// })
+
+app.use(function(req, res, next) {
+  console.log('1st middleware');
+  next();
+});
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next) {
+  // do logic with the req
+  // if it passes your test then you can call next()
+  // otherwise, return error
+  console.log('4th middleware');
+  next();
 });
 
-app.post('/movie/create', function (req, res, next) {
-  // do whatever
-  res.status(200).send('OK')
+function isAdmin (req, res, next) {
+  console.log('some logic with req to determine if admin');
+  next();
+}
+
+// ENDPOINTS
+app.get('/movies', isAdmin, MoviesController.get);
+
+// PATH PARAMS :variable
+app.get('/movies/find/:id', MoviesController.getOne);
+
+// // REQ.QUERY
+app.get('/movies/find', MoviesController.search);
+
+// QUERIES are NOT part of the PATH
+app.get('/movies/title/:title', function (req, res, next) {
+  console.log('movies', movies);
+  console.log('query', req.query, 'params', req.params);
+  for (var i = 0; i < movies.length; i++) {
+    if (movies[i].title === req.params.title) {
+      return res.status(200).send(movies[i]);
+    }
+  }
+  res.status(404).send('Not found');
 });
+
+app.post('/movies/create', MoviesController.create);
 
 app.put('/movies')
 
---
+++ models/movies.js
@@ -0,0 +1,17 @@
+module.exports = [{
+  "title": "Pride and Prejudice",
+  "director": "Jane Austen",
+  "genre": "Romance"
+}, {
+  "title": "Ocean's Eleven",
+  "director": "George Clooney",
+  "genre": "Action"
+}, {
+  "title": "Serenity",
+  "director": "Joss Whedon",
+  "genre": "Sci-Fi"
+}, {
+  "title": "Kill Bill",
+  "director": "Quentin Tarentino",
+  "genre": "Action"
+}];
